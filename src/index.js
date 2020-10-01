c12282d95ca4556879aed522f4c201e1ad353608
Ha Nguyen <mjboy1412@gmail.com>
2020-10-01 16:33:28 +0700

    init project to study

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import 'semantic-ui-css/semantic.min.css';
+import { Provider } from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
+import {configureStore} from './store/configureStore';
 import * as serviceWorker from './serviceWorker';
+
+const store = configureStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+      <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
