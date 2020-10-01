5ed3d2b5d4d7788933baafed3bc6d267903a5796
Ha Nguyen <mjboy1412@gmail.com>
2020-10-01 18:54:34 +0700

    add file to store

---
+++ src/store/configureStore.js
@@ -0,0 +1,14 @@
+﻿import {applyMiddleware, compose, createStore} from 'redux';
+import reducer from './reducers';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './sagas';
+
+export function configureStore () {
+    const sagaMiddleWare = createSagaMiddleware();
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    const store =  createStore(reducer, composeEnhancers(
+        applyMiddleware(sagaMiddleWare)
+    ));
+    sagaMiddleWare.run(rootSaga);
+    return store;
+}
