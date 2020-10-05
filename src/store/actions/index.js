fea4fbe3bfb596e3ad461ab32fc8d5428adfef4b
Ha Nguyen <mjboy1412@gmail.com>
2020-10-05 18:51:07 +0700

    write reducer

---
+++ src/store/actions/index.js
@@ -0,0 +1,20 @@
+﻿export const REQUEST = 'REQUEST';
+export const SUCCESS = 'SUCCESS';
+export const FAILURE = 'FAILURE';
+
+export function createRequestType (base) {
+    if(!base) {
+        throw new Error('cannot create request type with the = \'\' or base = null');
+    }
+    return [REQUEST, SUCCESS, FAILURE].reduce((acc,type) => {
+        acc[type] = `${base}_${type}`;
+        return acc;
+    },{})
+};
+
+export function createAction (type, payload = {}){
+    return {
+        type,
+        ...payload,
+    };
+};
---
+++ src/store/reducers/channels.js
@@ -0,0 +1,63 @@
+﻿import {VIDEO_DETAILS, WATCH_DETAILS} from '../actions/watch';
+import {SUCCESS} from '../actions';
+import {CHANNEL_LIST_RESPONSE} from '../api/youtube-api-response-types';
+
+const initialState = {
+    byId: {
+
+    }
+};
+
+export default function (state = initialState, action) {
+    switch(action.type) {
+        case WATCH_DETAILS[SUCCESS]: 
+            return reduceWatchDetails(action.response, state);
+        case VIDEO_DETAILS[SUCCESS]:
+            return reduceVideoDetails(action.response, state);
+        default:
+            return state;        
+    }
+};
+
+function reduceWatchDetails(response, prevState) {
+    const channelResponse = response.find(response => response.result.kind === CHANNEL_LIST_RESPONSE);
+    let channels = {};
+    if(channelResponse && channelResponse.result.time) {
+    // we know that there will only be one item
+    // because we ask for a channel with a specific id
+        const channel = channelResponse.result.items[0];
+        channels[channel.id] = channel;
+    }
+    return {
+        ...prevState,
+        byId: {
+            ...prevState.byId,
+            ...channels
+        }
+    }
+}
+
+function reduceVideoDetails(responses, prevState) {
+    const channelResponse = responses.find(response => response.result.kind === CHANNEL_LIST_RESPONSE);
+    let channelEntry = {};
+    if (channelResponse && channelResponse.result.items) {
+        const channel = channelResponse.result.items[0];
+        channelEntry = {
+            [channel.id]: channel,
+        }
+    }
+    return {
+        ...prevState.byId,
+        ...channelEntry,
+    }
+};
+
+/*
+*   Selectors
+* */
+
+export const getChannel = (state, channelId) => {
+    if(!channelId) return null;
+    return state.channels.byId[channelId];
+};
+
